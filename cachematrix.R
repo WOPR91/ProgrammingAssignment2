5a33411f5f3930758ef7323ceb9e8211ae73639f
WOPR91 <eric.hobson@jt4llc.com>
2025-04-24 15:51:03 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Exercises caching calculated variables
 
-## Write a short comment describing this function
+## Seems to make an object whcih caches values in the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<-NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m<<- inverse
+  getinverse <- function() m
+  
+  list(set = set, get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Uses cached values
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
